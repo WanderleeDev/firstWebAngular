10ac35370818e8f43d5b721ecffe1c051f26a536
Wanderlee Max <wanderleedev@gmail.com>
2023-07-18 23:50:13 -0500

    avance del formulario de registro

--- src/app/registration-table/registration-table.component.ts
+++ src/app/registration-table/registration-table.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { InputData } from '../interface/inputData.interface';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-registration-table',
@@ -16,13 +19,28 @@ export class RegistrationTableComponent {
     { name: 'description' , val:''}
   ];
 
+  form = new FormGroup({
+    item: new FormControl( '' ,[Validators.required]),
+    date: new FormControl( '' ,[Validators.required]),
+    flag: new FormControl( '' ,[Validators.required]),
+    data: new FormControl( '' ,[Validators.required]),
+    description: new FormControl(' ',[Validators.required])
+  });
+
 
   handlerSubmit(event:Event){
     event.preventDefault();
     const validator = this.listInput.every((value)=> Boolean(value.val));
 
     try {
-      if (validator) {console.log(this.listInput);}
+      if (validator) {
+        console.log(this.listInput);
+        this.saveLocal();
+      }
     } catch (error) {console.error(error);}
+  }
+
+  saveLocal(){
+    localStorage.setItem('data',JSON.stringify(this.listInput));
   }
 }
